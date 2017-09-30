09570b0cb8c82d197f2d761d447432c30f84dabc
boszkl8er <kittipong.iti17@gmail.com>
2017-09-30 21:01:30 +0700

    1

--- tempImportantNode.js
+++ tempImportantNode.js
@@ -2,5 +2,8 @@
 
 var clientID = '0a07a77d-ee2e-477e-93ca-fd7f59421237';
 var clientSecret = 'Km3aaZ+MavtvfXagfIHQ6bzFWfkXCXZgM8SGJ6VptyI=';
+
+
+
 var tokenLink = 'https://login.microsoftonline.com/3e0b6cbd-4959-4d01-81bf-ce883ddabd96/oauth2/token;
 var authorizeLink = 'https://login.microsoftonline.com/3e0b6cbd-4959-4d01-81bf-ce883ddabd96/oauth2/authorize';
