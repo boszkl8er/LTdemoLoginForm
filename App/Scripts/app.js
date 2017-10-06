e6679e220cf6af140dfa6d51cd508f2ffb1f63f9
boszkl8er <kittipong.iti17@gmail.com>
2017-10-06 10:03:08 +0700

    d

---
+++ App/Scripts/app.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+angular.module('catApp', ['ngRoute','AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "homeCtrl",
+        templateUrl: "/App/Views/Home.html",
+    }).when("/CatList", {
+        controller: "catListCtrl",
+        templateUrl: "/App/Views/CatList.html",
+        requireADLogin: true,
+    }).when("/UserData", {
+        controller: "userDataCtrl",
+        templateUrl: "/App/Views/UserData.html",
+    }).otherwise({ redirectTo: "/Home" });
+
+    adalProvider.init(
+        {
+            instance: 'https://login.microsoftonline.com/', 
+            //tenant: 'kvaes.onmicrosoft.com',
+			tenant: 'together.at',
+            //clientId: '9fb35525-a872-4bbd-a973-6165b6ba22aa',
+			clientId: '0a07a77d-ee2e-477e-93ca-fd7f59421237',
+            extraQueryParameter: 'nux=1',
+            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+        },
+        $httpProvider
+        );
+   
+}]);
---
+++ App/Scripts/catListSvc.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+angular.module('catApp')
+.factory('catListSvc', ['$http', function ($http) { 
+	//var apiUrl = 'https://spaauthdemoapi.kvaes.be';
+	var apiUrl = 'https://lfdemoapi.portal.azure-api.net';
+    return {
+        getItems : function(){
+            return $http.get(apiUrl + '/api/CatList');
+        },
+        getItem : function(id){
+            return $http.get(apiUrl + '/api/CatList/' + id);
+        },
+        postItem : function(item){
+            return $http.post(apiUrl + '/api/CatList/',item);
+        },
+        putItem : function(item){
+            return $http.put(apiUrl + '/api/CatList/', item);
+        },
+        deleteItem : function(id){
+            return $http({
+                method: 'DELETE',
+                url: apiUrl + '/api/CatList/' + id
+            });
+        }
+    };
+}]);
